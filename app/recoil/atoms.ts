56d312ac3c6b7ee15d824d8e2f2eafbf465006b5
ankit <ujjawalankit56023@gmail.com>
2025-02-09 00:05:18 +0530

    recoil added

---
+++ app/recoil/atoms.ts
@@ -0,0 +1,13 @@
+
+
+import { atom } from "recoil";
+
+export const userdetails=atom({
+    key:"userdetails",
+    default:null,
+})
+
+export const user=atom({
+    key:"user",
+    default:null,
+})
