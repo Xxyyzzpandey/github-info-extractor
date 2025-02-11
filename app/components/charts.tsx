eae23f96748ea2f109239e5f18f92184c71fa4d3
ankit <ujjawalankit56023@gmail.com>
2025-02-11 12:00:57 +0530

    chart added

---
+++ app/components/charts.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import { useLangStore } from "../store/store";
+
+const COLORS = ["#FF5733", "#FFBD33", "#33FF57", "#3357FF", "#FF33A1"];
+
+export default function PieChartComponent() {
+  const { langDetails } = useLangStore();
+  console.log(langDetails); // Debugging
+
+  // Convert data from { language, percentage } to { name, value }
+  const formattedData = langDetails.map((item) => ({
+    name: item.language,
+    value: (item.percentage *100)/100, // Convert decimal to percentage
+  }));
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      {formattedData.length > 0 ? (
+        <PieChart width={400} height={400}>
+          <Pie
+            data={formattedData}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            outerRadius={120}
+            fill="#8884d8"
+            dataKey="value"
+            label={({ name, value }) => `${name} ${value.toFixed(0)}%`}
+          >
+            {formattedData.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend />
+        </PieChart>
+      ) : (
+        <p className="text-center text-xl">Loading...</p>
+      )}
+    </div>
+  );
+}
--- app/store/store.ts
+++ app/store/store.ts
@@ -1,13 +1,33 @@
+import { details } from "motion/react-client";
 import {create} from "zustand"
 
 interface UserDetails {
     login?: string;
+    username?:string;
+    location?:string;
     name?: string;
     avatar_url?: string;
     bio?: string;
     public_repos?: number;
     followers?: number;
     following?: number;
+    activedays?:number;
+    language?:string;
+    stars?:number
+    updated_at?:Date;
+    created_at?:Date;
+  }
+
+  interface RepoDetails {
+     name:string,
+     url:string,
+     language:string,
+     stars:number
+  }
+
+  interface LangDetails{
+    language:string,
+    percentage:number
   }
 
 interface UserState {
@@ -19,3 +39,24 @@ export const useUserStore = create<UserState >((set) => ({
     userDetails: {}, // Initialize as empty object
     setUserDetails: (details) => set({ userDetails: details }), 
   }));
+
+
+  interface RepoState{
+    repoDetails:RepoDetails[];
+    setRepoDetails:(details:RepoDetails[])=>void;
+  }
+
+  export const useRepoStore=create<RepoState>((set)=>({
+    repoDetails:[],
+    setRepoDetails:(details)=>set({repoDetails:details}),
+  }))
+
+  interface LangState{
+    langDetails:LangDetails[];
+    setLangDetails:(details:LangDetails[])=>void;
+  }
+
+  export const useLangStore=create<LangState>((set)=>({
+    langDetails:[],
+    setLangDetails:(details)=>set({langDetails:details}),
+  }))
