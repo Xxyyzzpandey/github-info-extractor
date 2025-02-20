62e77060bdfcd6ee745d95427bf4da73109701f3
ankit <ujjawalankit56023@gmail.com>
2025-02-20 23:40:16 +0530

    add auth

---
+++ app/components/ui/loading.tsx
@@ -0,0 +1,9 @@
+
+export default function Spinner(){
+
+    return<>
+     <div className="flex items-center bg-slate-800 justify-center h-screen">
+      <div className="w-16 h-16 border-4 border-transparent border-t-blue-500 rounded-full animate-spin"></div>
+    </div>
+    </>
+}
